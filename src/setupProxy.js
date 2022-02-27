ad5f7bcaeadb5280afe40db8234fd0f2fbe2b11a
saeed <srcgrp@gmail.com>
2022-02-27 14:56:14 +0330

    fix(endpoint): fix endpoint

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,12 +4,9 @@ module.exports = function(app) {
   app.use(
     '/browser',
     createProxyMiddleware({
-      target: 'http://2.144.6.32:11800/',
-      pathRewrite: {
-        "^/browser": ""
-      },
-      changeOrigin: false,
-      secure: false
+      target: 'http://2.144.6.32:12800',
+      changeOrigin: true,
     })
   );
+
 };
